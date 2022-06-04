745800dd5b5b4b4ed40ffba54d4445017c26762d
Bobrov Andrey <andrey.bobrov88@bk.ru>
2022-06-04 14:53:08 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 15_425;
+        int miles = price / 20;
+        System.out.println("Бонусные милли " + miles);
+    }
+}
